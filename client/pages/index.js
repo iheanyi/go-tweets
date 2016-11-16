07e7932d22ef7649c48a57455ee4092fa396c140
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-11-15 21:12:31 -0500

    Begin messing around with the client-side business with websockets.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import MapGL from 'react-map-gl';
 
 export default class extends React.Component {
+  componentDidMount() {
+    console.log("In Component Did Mount Call!");
+    if (window.WebSocket) {
+      console.log("We got support for websockets!");
+      let conn = new WebSocket("ws://localhost:1337/ws");
+
+      conn.onopen = (evt) => {
+        console.log("Opened Socket.");
+        conn.send("This is an example message for stuff.");
+      }; 
+
+      conn.onerror = (evt) => {
+        console.log("We got an error, folks.");
+        console.log(evt);
+        console.log(evt.data);
+      };
+      
+      conn.onclose = (evt) => {
+        console.log("Closing out connection!");
+      };
+
+      conn.onmessage = (evt) => {
+        // Handle Messages here
+        console.log(evt.data);
+      };
+    }
+  }
   render() {
     return (
-      <MapGL width={400} height={400} latitude={37.7577} longitude={-122.4376}
-  zoom={8} onChangeViewport={(viewport) => {
-    const {latitude, longitude, zoom} = viewport;
-    // Optionally call `setState` and use the state to update the map.
-  }}
-  />);
+      <div>Hey Young World</div>
+      );
   }
 }
 //export default () => <div>Hello World!</div>
